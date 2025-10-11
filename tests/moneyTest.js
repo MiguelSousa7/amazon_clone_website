8112e72ac53917a3efd297cfd570a06178572bc2
Miguel Sousa <miguelsousabrg@gmail.com>
2025-10-11 15:22:19 +0100

    create automated tests for formatCurrency

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,6 +1,32 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+console.log("---- test suite: formatCurrency ----");
+
+// basic tests
+console.log("Converts cents into dollars");
 if (formatCurrency(2095) === "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+// edge tests
+console.log("Only converts to two decimals");
+if (formatCurrency(0) === "0.000") {
+  console.log("failed");
+} else {
+  console.log("passed");
+}
+
+console.log("Rounds up to the nearest cent");
+if (formatCurrency(200.5) === "2.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("Rounds down to the nearest cent");
+if (formatCurrency(200.3) === "2.00") {
   console.log("passed");
 } else {
   console.log("failed");
