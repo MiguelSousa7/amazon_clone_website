214cf6804fed64a219a95ed36ba48b385658fd4d
Miguel Sousa <miguelsousabrg@gmail.com>
2025-11-03 21:57:53 +0000

    practice using async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,18 @@ import { loadCart } from "../data/cart.js";
 //import "../data/cart-class.js";
 //import "../data/be-practice.js";
 
+async function loadPage() {
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
 /*
 Promise.all([
   loadProductsFetch(),
